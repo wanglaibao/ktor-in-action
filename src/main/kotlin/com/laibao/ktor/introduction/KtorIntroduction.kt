cbae5dbdc6df99594f54047e34fb2b93a8c946fc
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-18 01:48:02 +0800

    update pom.xml file info

--- src/main/kotlin/com/laibao/ktor/introduction/KtorIntroduction.kt
+++ src/main/kotlin/com/laibao/ktor/introduction/KtorIntroduction.kt
@@ -1,2 +1,32 @@
 package com.laibao.ktor.introduction
 
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            plainTextRoute()
+            htmlRoute()
+        }
+    }
+    server.start(wait = true)
+}
+
+fun Routing.plainTextRoute() {
+    get("/plain") {
+        call.respondText("Hello World!")
+    }
+}
+
+fun Routing.htmlRoute() {
+    get("/html") {
+        call.respondText("<h1>Hello World!</h1>", contentType = ContentType.Text.Html)
+    }
+}
